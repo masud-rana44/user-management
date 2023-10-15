be0c7d678f4ccd845b0fa0a48727140db3d8d5a2
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-15 14:30:26 +0600

    feat: Implemented add user form

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,10 +1,10 @@
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full border-4 border-indigo-600">
+    <div className="flex h-full flex-col border-4 border-indigo-600">
       <header className="bg-indigo-600 py-4 text-center text-xl  font-semibold text-white">
         User Management System
       </header>
-      <main>{children}</main>
+      <main className="flex-1 bg-gray-200">{children}</main>
     </div>
   );
 };
--- app/(routes)/users/page.tsx
+++ app/(routes)/users/page.tsx
@@ -1,5 +1,149 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be 2 character long",
+  }),
+  email: z.string().email("Please enter a valid email address"),
+  gender: z.enum(["male", "female"]),
+  status: z.enum(["active", "inactive"]),
+});
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      gender: "male",
+      status: "active",
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <div className="p-6">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mx-auto w-full max-w-2xl space-y-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Gender</FormLabel>
+                <FormControl className="flex gap-x-8">
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-row items-center space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="male" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Male</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="female" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Female</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Gender</FormLabel>
+                <FormControl className="flex gap-x-8">
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-row items-center space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="active" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Active</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="inactive" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Inactive</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className=" w-full bg-indigo-600 hover:bg-indigo-700"
+          >
+            Add User
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default UsersPage;

ad9a8cb3e13a421db269781f30ac78b35a9f07c1
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-15 16:15:14 +0600

    Integrated prisma

--- app/(routes)/users/page.tsx
+++ app/(routes)/users/page.tsx
@@ -7,16 +7,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import axios from "axios";
+import { useToast } from "@/components/ui/use-toast";
 
 const formSchema = z.object({
   name: z.string().min(2, {
@@ -28,6 +29,7 @@ const formSchema = z.object({
 });
 
 const UsersPage = () => {
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,15 +40,49 @@ const UsersPage = () => {
     },
   });
 
-  const onSubmit = () => {};
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post("/api/users", {
+        values,
+      });
+
+      toast({
+        variant: "default",
+        duration: 3000,
+        description: "User successfully created",
+      });
+    } catch (err: any) {
+      console.log(err);
+      toast({
+        variant: "destructive",
+        duration: 3000,
+        title: err?.message || "Something went wrong.",
+      });
+    }
+  };
 
   return (
     <div className="p-6">
+      <Link
+        href="/"
+        className="mb-10 block text-sm text-indigo-600 hover:underline"
+      >
+        {"<< All Users"}
+      </Link>
+
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="mx-auto w-full max-w-2xl space-y-4"
         >
+          <div className="text-center">
+            <h1 className="text-2xl font-medium">New User</h1>
+            <p className="text-sm text-gray-600">
+              Use the below form to create a new user
+            </p>
+          </div>
           <FormField
             control={form.control}
             name="name"
@@ -54,7 +90,7 @@ const UsersPage = () => {
               <FormItem>
                 <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input type="text" {...field} />
+                  <Input type="text" disabled={isLoading} {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -67,7 +103,7 @@ const UsersPage = () => {
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input type="email" {...field} />
+                  <Input disabled={isLoading} type="email" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -81,19 +117,20 @@ const UsersPage = () => {
                 <FormLabel>Gender</FormLabel>
                 <FormControl className="flex gap-x-8">
                   <RadioGroup
+                    disabled={isLoading}
                     onValueChange={field.onChange}
                     defaultValue={field.value}
                     className="flex flex-row items-center space-y-1"
                   >
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="male" />
+                        <RadioGroupItem disabled={isLoading} value="male" />
                       </FormControl>
                       <FormLabel className="font-normal">Male</FormLabel>
                     </FormItem>
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="female" />
+                        <RadioGroupItem disabled={isLoading} value="female" />
                       </FormControl>
                       <FormLabel className="font-normal">Female</FormLabel>
                     </FormItem>
@@ -112,19 +149,20 @@ const UsersPage = () => {
                 <FormLabel>Gender</FormLabel>
                 <FormControl className="flex gap-x-8">
                   <RadioGroup
+                    disabled={isLoading}
                     onValueChange={field.onChange}
                     defaultValue={field.value}
                     className="flex flex-row items-center space-y-1"
                   >
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="active" />
+                        <RadioGroupItem disabled={isLoading} value="active" />
                       </FormControl>
                       <FormLabel className="font-normal">Active</FormLabel>
                     </FormItem>
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="inactive" />
+                        <RadioGroupItem disabled={isLoading} value="inactive" />
                       </FormControl>
                       <FormLabel className="font-normal">Inactive</FormLabel>
                     </FormItem>
@@ -134,12 +172,15 @@ const UsersPage = () => {
               </FormItem>
             )}
           />
-          <Button
-            type="submit"
-            className=" w-full bg-indigo-600 hover:bg-indigo-700"
-          >
-            Add User
-          </Button>
+          <div>
+            <Button
+              type="submit"
+              disabled={isLoading}
+              className="mt-10 block w-full bg-indigo-600 hover:bg-indigo-700"
+            >
+              Add User
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
